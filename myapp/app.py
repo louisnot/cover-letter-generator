58b7795ab7036788c400d294e3ceeaf0fcda29c2
louisnot <lol-fun@hotmail.fr>
2023-07-02 16:15:29 +0200

    update app

--- myapp/app.py
+++ myapp/app.py
@@ -1,35 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 from flask_cors import CORS; 
 from lettre import generate_letter
 from resume_parser import parse_resume
 from pdfminer.high_level import extract_text
 import os
 
+main = Blueprint('main', __name__)
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config['MAX_CONTENT_LENGTH'] = 2048 * 2048
+    app.config['UPLOAD_EXTENSIONS'] = ['.pdf']
 
-app = Flask(__name__)
-CORS(app)
-app.config['MAX_CONTENT_LENGTH'] = 2048 * 2048
-app.config['UPLOAD_EXTENSIONS'] = ['.pdf']
+    app.register_blueprint(main)
 
-@app.route("/")
+    return app
+
+@main.route("/")
 def home():
     return "Welcome to cover letter API official page"
 
 # TODO change argument to have a list of messages from front #
-@app.route("/create-letter", methods=["POST"])
+@main.route("/create-letter", methods=["POST"])
 def create_letter():
     data = request.get_json()
     language = request.args.get("lng")
     letter = generate_letter(data,False,language);   
     return jsonify(letter[7:]), 201
 
-@app.route("/parse-resume", methods=["POST"])
+@main.route("/parse-resume", methods=["POST"])
 def file_to_resume():
     file = request.files['file']
     if file.filename != '':
         file_ext = os.path.splitext(file.filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+        if file_ext not in main.config['UPLOAD_EXTENSIONS']:
             return "Wrong file format", 400
         else:
             #file.filename = os.path.join('resume/provided', 'resume')
@@ -39,4 +44,4 @@ def file_to_resume():
 
 #if you run the current python file
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    main.run(debug=True, port=8000)
